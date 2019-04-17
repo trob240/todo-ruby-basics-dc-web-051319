f5bca80a94be37b8e6d31ad5f5d313d64c347499
Trevahr Robertson <trevahr@yahoo.com>
2019-04-17 14:52:17 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,25 +1,26 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  value = value
 end
 
-def argue
-  
+def argue(same)
+  return same
 end
 
-def greeting
+def greeting(x,y)
 
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
 def pizza_party
